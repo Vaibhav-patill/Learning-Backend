72cb15f3eda8885fcee9a67353e906f0e0e6b9ae
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-03-03 23:51:00 +0530

    learned styling using tailwind

---
+++ airbnb styling/routes/userRouter.js
@@ -0,0 +1,16 @@
+//Core modules
+const path=require('path');
+
+//External modules
+const express = require('express');
+const userRouter=express.Router();
+
+//Local module
+const rootDir = require("../utils/pathUtil");
+
+
+userRouter.get("/",(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','home.html'));
+}) 
+
+module.exports=userRouter;
