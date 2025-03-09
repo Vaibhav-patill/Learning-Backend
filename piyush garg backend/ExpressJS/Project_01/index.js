198c10793d24e484274354ccc8653cfc4cae549b
Vaibhav-patill <vaibhavpatil00913@gmail.com>
2025-03-09 16:05:46 +0530

    learned how to interact with mongodb using mongoose

--- piyush garg backend/ExpressJS/Project_01/index.js
+++ piyush garg backend/ExpressJS/Project_01/index.js
@@ -1,78 +1,112 @@
 const express = require("express");
 const fs = require("fs");
-const users = require("./MOCK_DATA.json");
-const { json } = require("stream/consumers");
-const { nextTick } = require("process");
+const mongoose = require("mongoose");
+const { type } = require("os");
 
 const app = express();
 const PORT = 8000;
 
+//Connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/youtube-app-1")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo Error", err));
+
+//schema
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobtitle: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+//model
+const User = mongoose.model("user", userSchema);
+
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   console.log("come in first middleware");
   // return res.json({mgs:"first middleware"});
   next();
-})
+});
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   console.log("come in first middleware");
   // return res.json({mgs:"first middleware"});
   next();
-})
- 
+});
+
 //Routes
-app.get("/users", (req, res) => {
+app.get("/users", async(req, res) => {
+  const allDbUsers = await User.find({})
   res.send(`
     <h1>Users List</h1>
     <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${allDbUsers.map((user) => `<li>${user.first_name}- ${user.email}</li>`).join("")}
     </ul>
     `);
 });
 
-app.get("/api/users", (req, res) => {
-  res.setHeader('myName','Vaibhav Patil');
-  console.log(req.headers);
-  return res.json(users);
+app.get("/api/users",async (req, res) => {
+  const allDbUsers=await User.find({});
+  return res.json(allDbUsers);
 });
 
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+  .get(async(req, res) => {
+    const user = await User.findById(req.params.id);
     return res.json(user);
   })
-  .patch((req, res) => {
-    return res.json({ status: "pending" });
+  .patch(async(req, res) => {
+    await User.findByIdAndUpdate(req.params.id,{last_name:"Changed"});
+    return res.json({status:"success"});
+
   })
-  .delete((req, res) => {
-    const id = Number(req.params.id);
-    const filteredUsers = users.filter((user) => user.id !== id);
+  .delete(async(req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({status:"success"});
+  });
 
-    if (filteredUsers.length === users.length) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Save updated data to file
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(filteredUsers, null, 2), (err) => {
-      if (err) return res.status(500).json({ message: "Error saving file" });
-      res.json({ message: "User deleted" });
-  });})
-
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
-  users.push({
-    ...body,
-    id: users.length + 1,
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.jobtitle
+  ) {
+    res.status(400).json("All fields required");
+  }
+  const result = await User.create({
+    first_name: body.first_name,
+    last_name: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobtitle: body.jobtitle,
   });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    res.json({ status: "success", id: users.length });
-  });
+
+  console.log(result);
+
+  return res.status(201).json({ msg: "success" });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`server started on port http://localhost:${PORT}`);
